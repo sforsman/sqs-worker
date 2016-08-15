0c6e6443e72a2ea92ed4717cc85313412f08eaf5
Sebastian Forsman <sebastian.forsman@avoine.fi>
2016-08-15 10:36:39 +0300

    Send (n)ack based on result

--- src/SQSWorker/Worker.php
+++ src/SQSWorker/Worker.php
@@ -43,13 +43,13 @@ class Worker
 
     // Main loop
     while($this->client->loop()) {
-      try { 
+      try {
         if($this->had_job) {
           echo "Waiting for work\n";
           $this->had_job = false;
         }
         // Expected to block
-        $this->client->receive();        
+        $this->client->receive();
       } catch (\Exception $e) {
         // Just rethrow to see what comes
         file_put_contents("php://stderr", "-> FATAL: Message receive failed, worker dying on ".get_class($e). ": ".$e->getMessage()."\n");
@@ -57,11 +57,11 @@ class Worker
       }
     }
   }
-  
+
   public function processResult($result)
   {
     $this->had_job = true;
-    
+
     if(count($result) === 0) {
       // Silently skipping (meant for supporting long poll timeouts)
       return;
@@ -108,19 +108,25 @@ class Worker
     }
 
     // We don't care about the return value
+    $failed = false;
     echo "-> Executing {$data['Function']}\n";
     try {
       unset($data['Function'], $data['Parameters']);
       call_user_func($callable, $parms, $data);
     } catch(\Exception $e) {
       file_put_contents("php://stderr", "-> ERROR: Executor threw an ".get_class($e). ": ".$e->getMessage()."\n");
+      $failed = true;
     }
-    
+
     // Currently we ack even if the executor crashes
     try {
-      $this->client->ack($tag);
+      if($failed) {
+        $this->client->nack($tag);
+      } else {
+        $this->client->ack($tag);
+      }
     } catch(\Exception $e) {
-      file_put_contents("php://stderr", "-> FATAL: Message deletion failed, worker dying on ".get_class($e). ": ".$e->getMessage()."\n");
+      file_put_contents("php://stderr", "-> FATAL: Message (n)ack failed, worker dying on ".get_class($e). ": ".$e->getMessage()."\n");
       exit(2);
     }
   }
